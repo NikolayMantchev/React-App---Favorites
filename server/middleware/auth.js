b5ad4bc0241bb6b8f0df699b6cda4d7fac60545a
Mantchev <nik.mantchev@gmail.com>
2023-02-17 11:41:56 +0100

    port

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,34 +1,34 @@
 import jwt from "jsonwebtoken";
 
-const secret = "test";
+const secret = process.env.secret;
 
 const auth = async (req, res, next) => {
-    // console.log('headers', req.headers)
-    try {
-        const token = req?.headers?.authorization?.split(" ")[1];
-        if (!token) {
-            return res
-                .status(401)
-                .json({ status: "error", message: "No valid token!" });
-        }
-        const isCustomAuth = token.length < 500;
+	// console.log('headers', req.headers)
+	try {
+		const token = req?.headers?.authorization?.split(" ")[1];
+		if (!token) {
+			return res
+				.status(401)
+				.json({ status: "error", message: "No valid token!" });
+		}
+		const isCustomAuth = token.length < 500;
 
-        let decodedData;
+		let decodedData;
 
-        if (token && isCustomAuth) {
-            decodedData = jwt.verify(token, secret);
+		if (token && isCustomAuth) {
+			decodedData = jwt.verify(token, secret);
 
-            req.userId = decodedData?.id;
-        } else {
-            decodedData = jwt.decode(token);
+			req.userId = decodedData?.id;
+		} else {
+			decodedData = jwt.decode(token);
 
-            req.userId = decodedData?.sub;
-        }
+			req.userId = decodedData?.sub;
+		}
 
-        next();
-    } catch (error) {
-        console.log(error);
-    }
+		next();
+	} catch (error) {
+		console.log(error);
+	}
 };
 
 export default auth;
